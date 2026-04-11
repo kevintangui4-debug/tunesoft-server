475ca842aca5713ab5453b9428225e506eee4c35
kevintangui4-debug <kevintangui4@gmail.com>
2026-04-11 21:33:19 +0200

    Update server.py
    Updated server to run on all interfaces and use environment variable for port.

--- server.py
+++ server.py
@@ -34,4 +34,5 @@ def check():
     return jsonify({"valid": True})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
